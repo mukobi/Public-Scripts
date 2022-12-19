c0f9ad189bb6ee77c68b756eaa6cd39addb34c8b
Gabriel Mukobi <gabrielmukobi@gmail.com>
2022-12-19 11:28:45 -0800

    Make first name prompt for filename and check multiple columns

--- Full Name to First Name/full_name_to_first_name.py
+++ Full Name to First Name/full_name_to_first_name.py
@@ -1,9 +1,7 @@
 import csv
 
-# Config
-INPUT_FILE = './research_symposium_reminder.csv'
-OUTPUT_FILE = f'{INPUT_FILE[:-4]}_with_first_name.csv'
-
+# File selection
+INPUT_FILE = input('Enter the path of a CSV file...\n')
 
 # Open the CSV file
 with open(INPUT_FILE, 'r') as file:
@@ -13,13 +11,17 @@ with open(INPUT_FILE, 'r') as file:
 
 # Calculate the stuff for each student
 for input_row in input_data:
-    if input_row['Name'] == '':
-        continue
-    input_row['first_name'] = input_row['Name'].split(' ')[0].strip()
+    for possible_name_key in ['Name', 'name', 'Full Name', 'full name', 'fullname', 'FullName']:
+        try:
+            full_name = input_row[possible_name_key]
+            if full_name != '':
+                input_row['first_name'] = full_name.split(' ')[0].strip()
+        except KeyError:
+            continue
 
 
 # Write output
-with open(OUTPUT_FILE, 'w', newline='\n') as file:
+with open(INPUT_FILE, 'w', newline='\n') as file:
     # Write the CSV data
     writer = csv.DictWriter(file, fieldnames=input_data[0].keys())
     writer.writeheader()
